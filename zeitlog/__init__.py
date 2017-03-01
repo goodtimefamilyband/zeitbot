90624876339682ca8c020a6a0c3574d13addf252
Andyman <ajo0131@gmail.com>
2017-03-01 02:35:57 -0500

    Move ChannelContainer to utils

--- zeitlog/__init__.py
+++ zeitlog/__init__.py
@@ -9,6 +9,7 @@ import time
 import pytz
 from collections import defaultdict
 import logbot
+from logbot.utils import ChannelContainer
 from app import bot
 
 from .schema import ScoreTbl, ScoreItem, Reaction
@@ -281,21 +282,6 @@ def getEmojiObj(server, emoji):
     if e is None:
         return emoji
     return e
-
-class ChannelContainer:
-
-    def __init__(self):
-        self.container = defaultdict(dict)
-        
-    # TODO: Use server ID instead
-    def __getitem__(self, channel):
-        return self.container[channel.server.name][channel.id]
-        
-    def __setitem__(self, channel, val):
-        self.container[channel.server.name][channel.id] = val
-    
-    def __contains__(self, channel):
-        return channel.id in self.container[channel.server.name]
     
 class Score:
     
