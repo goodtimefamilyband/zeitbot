3da8d2d0a49cb732112d20386c38ddea255e8c9a
Andyman <ajo0131@gmail.com>
2018-02-19 12:41:10 -0500

    Some rules refactoring

--- adminlog/classes.py
+++ adminlog/classes.py
@@ -1,3 +1,5 @@
+import discord
+from collections import defaultdict
 
 class Condition:
     def eval(self, *args):
@@ -19,15 +21,56 @@ class RuleBase:
         if self.condition.eval(*condargs):
             await self.action(*actargs)
             
-        
+
+'''            
 class MessageCountCondition(Condition):
 
-    def __init__(self, count):
+    def __init__(self, count, bot, server):
+        self.bot = bot
+        self.server = server
+        self.usercounts = defaultdict(int)
         self.count = count
+        
+        self.bot.loop.create_task(self.loop())
+        
+    def server_check(self, msg):
+        return self.server.id == msg.server.id
 
-    def eval(self, count):
+    async def loop(self):
+        while not self.bot.is_closed:
+            msg = await self.bot.wait_for_message(check=self.server_check)
+            self.bot.loop.create_task(self.update(message))
+        
+    async def update(self, message):
+        self.increase_user_count(message.author, 1)
+        
+    # TODO: modify for use with DB
+    def get_user_count(self, user):
+        return self.usercounts[user.id]
+        
+    def increase_user_count(self, user, amt):
+        self.usercounts[user.id] += amt
+        
+    def eval(self, user):
         print("Evaluating {} >= {} : {}".format(count, self.count, count >= self.count))
-        return count >= self.count
+        return self.get_user_count(user) >= self.count
+'''     
+        
+class ComplementCondition(Condition):
+    def __init__(self, condition):
+        self.condition = condition
+        
+    def eval(self, *args, **kwargs):
+        return !condition.eval(*args, **kwargs)
+        
+class HasRoleCondition(Condition):
+    def __init__(self, role):
+        self.role = role
+        
+    def eval(self, member):
+        mrole = discord.utils.find(lambda m : m.id == self.role.id, member.roles)
+        return mrole is not None
+        
         
 class AddRoleAction(Action):
     def __init__(self, bot, *roles, dbcheck = lambda u,r : (r)):
--- adminlog/schema.py
+++ adminlog/schema.py
@@ -10,13 +10,17 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, ForeignKey, Integer, String, Date, Float, Boolean
 
+import sys
+
 import discord
-from .classes import MessageCountCondition, AddRoleAction, RuleBase
+from .classes import Condition, Action, RuleBase
 
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=SQL_DEBUG)
 
-Session = sessionmaker(bind=engine)    
+Session = sessionmaker(bind=engine) 
+
+#TODO: use object_session() (or similar)?
 
 class Server(Base):
     __tablename__ = "servers"
@@ -35,18 +39,145 @@ class Member(Base):
     id = Column(String, primary_key=True)
     serverid = Column(String, ForeignKey('servers.id'), primary_key=True)
     msgcount = Column(Integer, default=0)
+
+class Role(Base):
+    __tablename__ = "roles"
     
+    id = Column(String, primary_key=True)
+    
+    def find_discord_role(self, server):
+        return discord.utils.find(lambda r : r.id == self.id, server.roles)
+        
+
+class RuleRole(Base):
+    __tablename__ = "rule_role"
+    
+    ruleid = Column(Integer, ForeignKey('rules.id'), primary_key=True)
+    roleid = Column(String, ForeignKey('roles.id'), primary_key=True)
+
+    
+class DBClass(Base):
+    __tablename__ = "classes"
+    
+    id = Column(Integer, primary_key=True)
+    classname = Column(String)
+#    datatbl = Column(String)
+    
+class ConditionBase(Condition):
+    id = Column(Integer, primary_key=True)
+    
+class ActionBase(Action)
+    id = Column(Integer, primary_key=True)
+    
+class MessageCountCondition(ConditionBase):
+
+    __tablename__ = "msgcounts"
+
+    count = Column(Integer, default=0)
+    serverid = Column(Integer, ForeignKey('servers.id'))
+
+    '''
+    def __init__(self, count, bot, server):
+        self.bot = bot
+        self.server = server
+        self.usercounts = defaultdict(int)
+        self.count = count
+        
+        self.bot.loop.create_task(self.loop())
+    '''
+        
+    def server_check(self, msg):
+        return self.server.id == msg.server.id
+
+    '''
+    async def loop(self):
+        while not self.bot.is_closed:
+            msg = await self.bot.wait_for_message(check=self.server_check)
+            self.bot.loop.create_task(self.update(message))
+        
+    async def update(self, message):
+        self.increase_user_count(message.author, 1)
+    '''
+    
+    # TODO: modify for use with DB
+    def get_user_count(self, user, db):
+        m = db.query(Member).filter_by(id=user.id)
+        return m.msgcount
+        
+    def increase_user_count(self, user, amt, db):
+        #self.usercounts[user.id] += amt
+        m = db.query(Member).filter_by(id=user.id)
+        m.msgcount += amt
+        db.add(m)
+        db.commit()
+        
+    def eval(self, user):
+        print("Evaluating {} >= {} : {}".format(count, self.count, count >= self.count))
+        return self.get_user_count(user) >= self.count
+    
+
+class AddRoleAction(ActionBase):
+    #def __init__(self, bot, *roles, dbcheck = lambda u,r : (r)):
+    serverid = Column(Integer, ForeignKey('servers.id'))
+    
+    def get_roles(self, db, server):
+        return [r.find_discord_role(server) for r in db.query(Role).join(RuleRole, Role.id == RuleRole.roleid).filter(RuleRole.ruleid == self.id)]
+    
+    async def __call__(self, dmember, db, client):
+        roles = [role for role in self.get_roles(db, dmember.server) if not role in dmember.roles]
+        #print("AddRoleAction {} {} {}".format([role.name for role in roles], self.roles, user.roles))
+        if len(roles) > 0:
+            await client.add_roles(user, *roles)
+
+class DBRule(Base):
+    __tablename__ = "rules"
+    
+    id = Column(Integer, primary_key=True)
+    serverid = Column(String, ForeignKey('servers.id'))
+    classname = Column(String)
+    
+    def get_rule(self, db):
+        return db.query(getattr(sys.modules[__name__], classname)).filter_by(id=self.id).first())
+
+class MessageCountRoleAddRule(Base, RuleBase):
+    __tablename__ = "messagecountrolerules":
+    
+    id = Column(Integer, ForeignKey('rules.id'), primary_key = True)
+    condid = Column(Integer)
+    actid = Column(Integer)
+    
+    def register(self, db):
+        
+    
+#TODO: Inherit from this? How to instantiate?    
 class Rule(Base, RuleBase):
     __tablename__ = "rules"
     
     id = Column(Integer, primary_key=True)
     serverid = Column(String, ForeignKey('servers.id'))
-    mincount = Column(Integer, default=0)
+    condtype = Column(String)
+    condid = Column(Integer)
+    acttype = Column(String)
+    actid = Column(Integer)
+    event = Column(String)
     
+    def get_condition(self, db):
+        #return db.query(getattr(sys.modules[__name__], self.condtype)).filter_by(id=self.condid).first()
+        return self.get_instance(db, self.condtype, self.condid)
+        
+    def get_action(self, db):
+        #return db.query(getattr(sys.modules[__name__], self.condtype)).filter_by(id=self.condid).first()
+        return self.get_instance(db, self.acttype, self.actid)
+        
+    def get_instance(self, db, classname, id):
+        return db.query(getattr(sys.modules[__name__], classname)).filter_by(id=id).first()
+    
+    
+    '''
     def load_rule(self, bot, db):
         self.condition = MessageCountCondition(self.mincount)
         server = discord.utils.find(lambda s : s.id == self.serverid, bot.servers)
-        roles = [r.find_role(server) for r in db.query(Role).join(RuleRole, Role.id == RuleRole.roleid).filter(RuleRole.ruleid == self.id)]
+        roles = [r.find_discord_role(server) for r in db.query(Role).join(RuleRole, Role.id == RuleRole.roleid).filter(RuleRole.ruleid == self.id)]
         
         print(roles)
         self.action = AddRoleAction(bot, *roles)
@@ -69,28 +200,16 @@ class Rule(Base, RuleBase):
             return "({}) Roles added after {} messages: {}".format(self.id, self.mincount, ",".join([r.name for r in self.action.roles]))
         except AttributeError:
             return ""
-        
+    '''    
 
-class Role(Base):
-    __tablename__ = "roles"
-    
-    id = Column(String, primary_key=True)
-    
-    def find_role(self, server):
-        return discord.utils.find(lambda r : r.id == self.id, server.roles)
-        
-class RuleRole(Base):
-    __tablename__ = "rule_role"
-    
-    ruleid = Column(Integer, ForeignKey('rules.id'), primary_key=True)
-    roleid = Column(String, ForeignKey('roles.id'), primary_key=True)
-    
+
+'''    
 class RoleException(Base):
     __tablename__ = "role_exceptions"
     
     ruleid = Column(Integer, ForeignKey('rules.id'), primary_key=True)
     roleid = Column(String, ForeignKey('roles.id'), primary_key=True)
     memberid = Column(String, ForeignKey('members.id'), primary_key=True)
-    
+'''    
     
 Base.metadata.create_all(engine)
